743f3db56ab7cae982ecf6051ca2b7be9b094bdc
Mehedi <mh20011999@gmail.com>
2020-08-19 11:33:30 +0600

    opening_page and search

---
+++ app/src/main/java/com/example/localbusfinder/all_bus.java
@@ -0,0 +1,20 @@
+package com.example.localbusfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class all_bus extends AppCompatActivity {
+    private Button bu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_bus);
+    }
+    //initialize button
+
+
+}
